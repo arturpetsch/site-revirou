b8545de482dae87b5789cd355252bc26d07182c2
arturpetsch <32885559+arturpetsch@users.noreply.github.com>
2025-07-27 19:21:02 -0300

    Add files via upload

--- pages/404.js
+++ pages/404.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default function Custom404() {
   return (
-    <div>
-      <h1>404 - Página não encontrada</h1>
-      <p>Volte para a <a href="/">página inicial</a>.</p>
-    </div>
-  );
+  <div>
+    {someObject.message} // ou qualquer propriedade
+    {/* ou */}
+    {JSON.stringify(someObject)}
+  </div>
+);
 }
--- pages/500.js
+++ pages/500.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default function Custom500() {
   return (
-    <div>
-      <h1>500 - Erro interno do servidor</h1>
-      <p>Tente novamente mais tarde ou contate o suporte.</p>
-    </div>
-  );
+  <div>
+    {someObject.message} // ou qualquer propriedade
+    {/* ou */}
+    {JSON.stringify(someObject)}
+  </div>
+);
 }
