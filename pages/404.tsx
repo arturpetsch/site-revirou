ef034c7d9884b4665872c8627dcfbf702d06a06c
arturpetsch <32885559+arturpetsch@users.noreply.github.com>
2025-07-27 19:24:11 -0300

    Add files via upload

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Custom404() {
+  return (
+  <div>
+    {someObject.message} // ou qualquer propriedade
+    {/* ou */}
+    {JSON.stringify(someObject)}
+  </div>
+);
+}
